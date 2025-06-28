36c016e8e0750d9ace8c1d38b126ee65a7fa54a8
Satiya <72025507+SatiyaPragaash@users.noreply.github.com>
2025-06-28 02:01:42 -0300

    feat: DynamoDM implementation

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+# -------------------- S3 BUCKET --------------------
 resource "aws_s3_bucket" "uploads" {
   bucket = "smart-expense-tracker-uploads-${random_id.suffix.hex}"
 
@@ -17,10 +22,24 @@ resource "aws_s3_bucket" "uploads" {
   force_destroy = true
 }
 
-resource "random_id" "suffix" {
-  byte_length = 4
+# -------------------- DYNAMODB TABLE --------------------
+resource "aws_dynamodb_table" "transactions" {
+  name         = "TransactionsTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Transactions Table"
+    Environment = "Dev"
+  }
 }
 
+# -------------------- IAM ROLE --------------------
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_execution_role"
   assume_role_policy = jsonencode({
@@ -47,51 +66,61 @@ resource "aws_iam_role_policy" "lambda_policy" {
           "logs:CreateLogStream",
           "logs:PutLogEvents"
         ],
-        Effect   = "Allow",
+        Effect = "Allow",
         Resource = "*"
       },
       {
-        Effect = "Allow",
         Action = [
           "s3:PutObject",
           "s3:GetObject",
           "s3:ListBucket"
         ],
+        Effect = "Allow",
         Resource = [
           aws_s3_bucket.uploads.arn,
           "${aws_s3_bucket.uploads.arn}/*"
         ]
+      },
+      {
+        Action = [
+          "dynamodb:PutItem"
+        ],
+        Effect = "Allow",
+        Resource = aws_dynamodb_table.transactions.arn
       }
     ]
   })
 }
 
-
+# -------------------- LAMBDA FUNCTION --------------------
 resource "aws_lambda_function" "upload_expense_lambda" {
-  function_name = "uploadExpenseHandler"
-  role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  filename = "${path.module}/lambda.zip"
+  function_name    = "uploadExpenseHandler"
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs16.x"
+  filename         = "${path.module}/lambda.zip"
   source_code_hash = filebase64sha256("${path.module}/lambda.zip")
-  timeout       = 30
+  timeout          = 30
+
   environment {
     variables = {
-      S3_BUCKET = aws_s3_bucket.uploads.bucket
+      S3_BUCKET     = aws_s3_bucket.uploads.bucket
+      DYNAMO_TABLE  = aws_dynamodb_table.transactions.name
     }
   }
 }
 
+# -------------------- API GATEWAY --------------------
 resource "aws_apigatewayv2_api" "api" {
   name          = "expense-api"
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.upload_expense_lambda.invoke_arn
-  integration_method = "POST"
+  api_id                 = aws_apigatewayv2_api.api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.upload_expense_lambda.invoke_arn
+  integration_method     = "POST"
   payload_format_version = "2.0"
 }
 
@@ -115,10 +144,15 @@ resource "aws_lambda_permission" "apigw_lambda" {
   source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
 
+# -------------------- OUTPUTS --------------------
 output "api_endpoint" {
   value = "${aws_apigatewayv2_api.api.api_endpoint}/upload"
 }
 
 output "s3_bucket_name" {
   value = aws_s3_bucket.uploads.bucket
+}
+
+output "dynamo_table_name" {
+  value = aws_dynamodb_table.transactions.name
 }
